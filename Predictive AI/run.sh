369e24af592e3aca5c21a2b7c0077a57e2edf61a
Eric Sun <2397286134@qq.com>
2024-10-31 01:02:19 -0700

    Keep anonymity of the code

--- Predictive AI/run.sh
+++ Predictive AI/run.sh
@@ -1,2 +1,2 @@
-export TRANSFORMERS_CACHE=/data/z9sun/.cache
-CUDA_VISIBLE_DEVICES=1 python predictive_ai_factor_score.py
+export TRANSFORMERS_CACHE=/...
+CUDA_VISIBLE_DEVICES=... python predictive_ai_factor_score.py
